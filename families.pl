ad69eca3431faa0623dab486422e6eaeae1b3944
Cristian <95005685+Criss117@users.noreply.github.com>
2024-05-06 14:54:12 -0500

    commit

--- families.pl
+++ families.pl
@@ -1,3 +1,5 @@
+?- use_module(library(date)).
+
 %padres
 father('Tomás García Pérez', '7 de Mayo de 1960', 'Profesor', 60).
 father('José Pérez Ruiz', '6 de Marzo de 1963', 'Pintor', 120).
@@ -17,7 +19,7 @@ children('José María Pérez Pérez', '12 de Julio de 1994', 'Estudiante', 0).
 family('Tomás García Pérez', 'Ana López Ruiz', ['Juan García López', 'María García López']).
 family('José Pérez Ruiz', 'Luisa Gálvez Pérez', ['Juan Luis Pérez Pérez', 'María José Pérez Pérez', 'José María Pérez Pérez']).
 
-
+%2.
 % Predicado para verificar si una familia tiene un número específico de hijos
 num_childrens(Num, Father, Mother) :-
   family(Father, Mother, Childrens),
@@ -28,23 +30,64 @@ num_childrens(Num) :-
   format('La familia formada por ~w y ~w tiene ~d hijos.~n', [Father, Mother, Num]); 
   format('No hay familias con ~d hijos', [Num]).
 
+% ¿existe familia sin hijos?
+%?- num_childrens(0).
+% ¿existe familia con un hijo?
+%?- num_childrens(1).
+% ¿existe familia con dos hijos?
+%?- num_childrens(2).
+% ¿existe familia con tres hijos?
+%?- num_childrens(3).
+% ¿existe familia con cuatro hijos.?
+%?- num_childrens(4).
+
+% 3. Buscar los nombres de los padres de familia con tres hijos.
+% ?- num_childrens(3).
+
+% 4
 % Predicado para verificar si una familia está casada
 married(Father, Mother) :-
   family(Father, Mother, _).
 
-man_married(Father) :-
+married_man(Father) :-
   married(Father, Mother) ->
   format('~w y ~w están casados ~n', [Father, Mother]); 
   format('~w no está casado ~n', [Father]). 
 
-woman_married(Mother) :-
+% Tomás García Pérez está casado
+% ?- married_man('Tomás García Pérez').
+% ?- married_man('John Doe').
+
+%5
+married_men :-
+  married(Father, Mother),
+  write(Father), write(' está casado con '), write(Mother), nl, fail.
+married_men :- true.
+
+% ?- married_men.
+
+%6
+married_woman(Mother) :-
   married(Father, Mother) ->
   format('~w y ~w están casados ~n', [Father, Mother]); 
   format('~w no está casada ~n', [Mother]). 
+% Luisa Gálvez Pérez estara casada
+%?- married_woman('Luisa Gálvez Pérez').
+%?- married_woman('Jane Doe').
 
+%7
+married_women :-
+  married(Father, Mother),
+  write(Mother), write(' está casada con '), write(Father), nl, fail.
+married_women :- true.
+
+% ?- married_women.
+
+
+%8 Determinar el nombre de todas las mujeres casadas que trabajan.
 mother_working(Mother) :-
   mother(Mother, _, Profession, _),
-  woman_married(Mother),
+  married_woman(Mother),
   Profession \= 'Estudiante'.
   
 list_mother_working :- 
@@ -53,12 +96,27 @@ list_mother_working :-
 
 list_mother_working :- true.
 
+%?- list_mother_working.
+
+%9.Definir la relación hijo(X) que se verifique si X figura en alguna lista de hijos.
 hijo(X) :-
   family(_, _, ListaHijos),
   member(X, ListaHijos) -> 
     format('~w es un hijo.~n', [X]); 
     format('~w no es un hijo.~n', [X]).
+% ?- hijo('Juan García López').
 
+% 10.Preguntar por los hijos.
+hijos :-
+  family(_, _, ListaHijos),
+  member(X, ListaHijos),
+  format('~w es un hijo.~n', [X]),
+  fail.
+hijos :- true.
+% ?- hijos.
+
+% 11.Definir la relación persona(X) que se verifique si X es una persona existente
+% en la base de datos.
 persona(X) :-
   father(X, _, _, _).
 persona(X) :-
@@ -70,81 +128,41 @@ existe(X) :-
   persona(X) -> 
   write('Es una persona existente en la base de datos'); 
   write('No es una persona existente en la base de datos').
+% ?- existe('Tomás García Pérez').
+
+% 12. Consulta para obtener los nombres y apellidos de todas las personas existentes
+% ?- persona(X), format('~w~n', [X]), fail.
 
 sueldo(X, Y) :- father(X, _, _, Y).
 sueldo(X, Y) :- mother(X, _, _, Y).
 
-% Definición de la relación total(L, Y)
-total([], 0). % Caso base: la suma de una lista vacía es 0.
-total([Persona | Resto], Suma) :-
-  sueldo(Persona, Salario), % Obtiene el salario de la persona.
-  total(Resto, SumaResto),  % Calcula la suma del resto de la lista.
-  Suma is Salario + SumaResto. % Calcula la suma total.
-
-% Calcula los ingresos totales de una familia
-ingresos_familia([], 0). % Caso base: la suma de una lista vacía es 0.
-ingresos_familia([Padre, Madre | Hijos], Ingresos) :-
-  sueldo(Padre, SalarioPadre), % Obtiene el salario del padre.
-  sueldo(Madre, SalarioMadre), % Obtiene el salario de la madre.
-  total(Hijos, IngresosHijos), % Calcula la suma de los ingresos de los hijos.
-  Ingresos is SalarioPadre + SalarioMadre + IngresosHijos. % Calcula la suma total de ingresos de la familia.
-
-% Consulta los ingresos totales de cada familia
-consulta_ingresos_familias :-
-  family(Padre, Madre, Hijos),
-  ingresos_familia([Padre, Madre | Hijos], Ingresos),
-  write('Familia: '), write(Padre), write(' y '), write(Madre), write(', Ingresos totales: '), write(Ingresos), nl,
-  fail. % Fuerza el backtracking para encontrar más soluciones
-consulta_ingresos_familias. % Termina la consulta
-
-% Ejemplo de uso:
-
-%2.
-% ¿existe familia sin hijos?
-%?- num_childrens(0).
-
-% ¿existe familia con un hijo?
-%?- num_childrens(1).
-
-% ¿existe familia con dos hijos?
-%?- num_childrens(2).
-
-% ¿existe familia con tres hijos?
-% 3. Buscar los nombres de los padres de familia con tres hijos.
-%?- num_childrens(3).
-
-% ¿existe familia con cuatro hijos.?
-%?- num_childrens(4).
-
-%4.
-% Tomás García Pérez está casado
-% ?- man_married('Tomás García Pérez').
-% ?- man_married('John Doe').
-
-%6
-% Luisa Gálvez Pérez estara casada
-%?- woman_married('Luisa Gálvez Pérez').
-%?- woman_married('Jane Doe').
-
-%8 Determinar el nombre de todas las mujeres casadas que trabajan.
-%?- list_mother_working.
-
-%9.Definir la relación hijo(X) que se verifique si X figura en alguna lista de hijos.
-% ?- hijo('Juan García López').
-
-% 11.Definir la relación persona(X) que se verifique si X es una persona existente
-% en la base de datos.
-% ?- existe('Tomás García Pérez').
-
-% Consulta para obtener los nombres y apellidos de todas las personas existentes
-% ?- persona(X), format('~w~n', [X]), fail.
 
 % 16.Definir la relación sueldo(X,Y) que se verifique si el sueldo de la persona X es Y.
+total([], 0).
+total([Persona | Resto], Suma) :-
+  sueldo(Persona, Salario), 
+  total(Resto, SumaResto), 
+  Suma is Salario + SumaResto. 
+
 % ?- sueldo('Tomás García Pérez', Salario), write(Salario).
 
 % 18.Definir la relación total(L,Y) de forma que si L es una lista de personas,
 % entonces Y es la suma de los sueldos de las personas de la lista L.
 % ?- total(['Tomás García Pérez', 'José Pérez Ruiz'], Suma), write(Suma).
 
+ingresos_familia([], 0). 
+ingresos_familia([Padre, Madre | Hijos], Ingresos) :-
+  sueldo(Padre, SalarioPadre), 
+  sueldo(Madre, SalarioMadre), 
+  total(Hijos, IngresosHijos),
+  Ingresos is SalarioPadre + SalarioMadre + IngresosHijos.
+
 % 20.Calcular los ingresos totales de cada familia.
-?- consulta_ingresos_familias.
+consulta_ingresos_familias :-
+  family(Padre, Madre, Hijos),
+  ingresos_familia([Padre, Madre | Hijos], Ingresos),
+  write('Familia: '), write(Padre), write(' y '), write(Madre), write(', Ingresos totales: '), write(Ingresos), nl,
+  fail. % Fuerza el backtracking para encontrar más soluciones
+  consulta_ingresos_familias. % Termina la consulta
+  
+% ?- consulta_ingresos_familias.
